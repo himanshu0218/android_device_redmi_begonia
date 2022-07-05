1bd7f4c570afef76e2e76efab98ea9896ef205a6
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2022-07-05 12:39:39 +0530

    Update and rename lineage_begonia.mk to arrow_begonia.mk

--- arrow_begonia.mk
+++ arrow_begonia.mk
@@ -16,8 +16,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_DOESNT_LIKE_FLIPENDO := true
 
@@ -50,3 +50,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+ARROW_GAPPS := true
